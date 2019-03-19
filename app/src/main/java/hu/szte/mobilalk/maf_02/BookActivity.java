2b7c3ad32f55e67c443beb215a5045d26b50e11f
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-03-19 13:30:59 +0100

    ora vege, mukodo kod

--- app/src/main/java/hu/szte/mobilalk/maf_02/BookActivity.java
+++ app/src/main/java/hu/szte/mobilalk/maf_02/BookActivity.java
@@ -1,6 +1,9 @@
 package hu.szte.mobilalk.maf_02;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
@@ -8,8 +11,10 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,6 +38,31 @@ public class BookActivity extends AppCompatActivity
     }
 
     public void startSearch(View view) {
+        String queryString = this.mBookInput.getText().toString();
+
+        InputMethodManager inputManager =
+                (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
+            mAuthorText.setText("");
+            mTitleText.setText("");
+        } else {
+            Toast.makeText(this, "no connection", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
